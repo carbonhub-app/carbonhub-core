f90b0a5dc67c5b44524811e19faa9dbe01f25168
Faiz A <faizathr.dev@proton.me>
2025-06-01 16:03:35 +0700

    fix: next development server CORS

--- app/server.js
+++ app/server.js
@@ -12,6 +12,7 @@ app.use(cors({
     origin: [
         'http://localhost:80',
         'http://localhost:8080',
+        'http://localhost:3000',
         'http://localhost:4173',
         'http://localhost:5173',
         'https://carbonhub.app',
